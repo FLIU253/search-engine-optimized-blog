6a56ad7e16e68768be5f17f0df61daa7b1e5881d
Franklin Liu <franklin.liu@autodata.net>
2019-11-07 13:10:47 -0500

    basic user signin added

---
+++ frontend/components/auth/SigninComponent.jsx
@@ -0,0 +1,88 @@
+import { Fragment, useState } from "react";
+import { signin } from "../../actions/auth";
+import Router from "next/router";
+
+const SigninComponent = () => {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+    error: "",
+    loading: false,
+    message: "",
+    showForm: true
+  });
+
+  const { email, password, error, loading, message, showForm } = values;
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setValues({ ...values, loading: true, error: false });
+    const user = { email, password };
+
+    signin(user).then(data => {
+      console.log(data);
+      if (data.error) {
+        setValues({ ...values, error: data.error });
+      } else {
+        //save user token to cookie
+        //save user info to localstorage
+        //authenticate user
+        Router.push(`/`);
+      }
+    });
+  };
+
+  const handleChange = value => e => {
+    setValues({ ...values, error: false, [value]: event.target.value });
+  };
+
+  const showLoading = () =>
+    loading ? <div className="alert alert-info">Loading...</div> : "";
+
+  const showError = () =>
+    error ? <div className="alert alert-danger">{error}</div> : "";
+
+  const ShowMessage = () =>
+    message ? <div className="alert alert-info">{message}</div> : "";
+
+  const signinForm = () => {
+    return (
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <input
+            value={email}
+            onChange={handleChange("email")}
+            type="email"
+            className="form-control"
+            placeholder="Type your email"
+          />
+        </div>
+
+        <div className="form-group">
+          <input
+            value={password}
+            onChange={handleChange("password")}
+            type="password"
+            className="form-control"
+            placeholder="Type your password"
+          />
+        </div>
+
+        <div>
+          <button className="btn btn-primary">Sign in</button>
+        </div>
+      </form>
+    );
+  };
+
+  return (
+    <Fragment>
+      {showError()}
+      {showLoading()}
+      {ShowMessage()}
+      {showForm && signinForm()}
+    </Fragment>
+  );
+};
+
+export default SigninComponent;
