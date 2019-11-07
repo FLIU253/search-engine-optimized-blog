750aa64ad55f190ef6318a38d052e1ae00b77674
Franklin Liu <franklin.liu@autodata.net>
2019-11-07 15:44:55 -0500

    categories api finished

--- backend/routes/category.js
+++ backend/routes/category.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { create } = require("../controllers/category");
+const { create, list, read, remove } = require("../controllers/category");
 //validators
 const { runValidation } = require("../validators");
 const { categoryCreateValidator } = require("../validators/category");
@@ -14,5 +14,9 @@ router.post(
   adminMiddleware,
   create
 );
+
+router.get("/categories", list);
+router.get("/category/:slug", read);
+router.delete("/category/:slug", requiresSignin, adminMiddleware, remove);
 
 module.exports = router;
