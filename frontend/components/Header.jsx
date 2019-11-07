987b75a93761287632ecf46b18617fc7c7bd62d8
Franklin Liu <franklin.liu@autodata.net>
2019-11-07 15:32:07 -0500

    category routes added

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -47,6 +47,22 @@ const Header = props => {
               </Fragment>
             )}
 
+            {isAuth() && isAuth().role === 0 && (
+              <NavItem>
+                <Link href="/user">
+                  <NavLink>{`${isAuth().name}'s Dashboard`}</NavLink>
+                </Link>
+              </NavItem>
+            )}
+
+            {isAuth() && isAuth().role === 1 && (
+              <NavItem>
+                <Link href="/admin">
+                  <NavLink>{`${isAuth().name}'s Dashboard`}</NavLink>
+                </Link>
+              </NavItem>
+            )}
+
             {isAuth() && (
               <NavItem>
                 <Link>
