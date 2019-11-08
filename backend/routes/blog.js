524994981037f12cc54b843288e77540df1333b1
Franklin Liu <franklin.liu@autodata.net>
2019-11-08 14:20:28 -0500

    implemented update, delete and get route for blog

--- backend/routes/blog.js
+++ backend/routes/blog.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const { create } = require("../controllers/blog");
+const {
+  create,
+  list,
+  listAllBlogsCategoriesTags,
+  read,
+  remove,
+  update
+} = require("../controllers/blog");
 const { requiresSignin, adminMiddleware } = require("../controllers/auth");
 
 router.post("/blog", requiresSignin, adminMiddleware, create);
+router.get("/blogs", list);
+router.post("/blogs-categories-tags", listAllBlogsCategoriesTags);
+router.get("/blog/:slug", read);
+router.delete("/blog/:slug", requiresSignin, adminMiddleware, remove);
+router.put("/blog/:slug", requiresSignin, adminMiddleware, update);
 
 module.exports = router;
