0ec8d0eff7d780afc0606977bbc0213300c9ddf3
Franklin Liu <franklin.liu@autodata.net>
2019-11-08 15:39:50 -0500

    added seo head section

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,6 +4,9 @@ module.exports = withCSS({
   publicRuntimeConfig: {
     APP_NAME: "SEOBLOG",
     API_DEVELOPMENT: "http://localhost:8000/api",
-    PRODUCTION: false
+    PRODUCTION: false,
+    DOMAIN_DEVELOPMENT: "http://localhost:3000",
+    DOMAIN_PRODUCTION: "",
+    FACE_APP_ID: "1033238083677412"
   }
 });
