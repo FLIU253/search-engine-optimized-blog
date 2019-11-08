243969ebdcf15f877e226ce6cb071125e7397f22
Franklin Liu <franklin.liu@autodata.net>
2019-11-08 09:15:25 -0500

    full implemented category section

--- frontend/components/crud/Category.jsx
+++ frontend/components/crud/Category.jsx
@@ -1,8 +1,13 @@
-import { useState, useEffect, Fragment } from "react";
+/**
+ * FULL CODE CATEGORY
+ * components/crud/Category.js
+ */
+
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import Router from "next/router";
-import { isAuth, getCookie } from "../../actions/auth";
-import { create } from "../../actions/category";
+import { getCookie } from "../../actions/auth";
+import { create, getCategories, removeCategory } from "../../actions/category";
 
 const Category = () => {
   const [values, setValues] = useState({
@@ -10,20 +15,85 @@ const Category = () => {
     error: false,
     success: false,
     categories: [],
-    removed: false
+    removed: false,
+    reload: false
   });
 
   const { name, error, success, categories, removed, reload } = values;
   const token = getCookie("token");
 
+  useEffect(() => {
+    loadCategories();
+  }, [reload]);
+
+  const loadCategories = () => {
+    getCategories().then(data => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        setValues({ ...values, categories: data });
+      }
+    });
+  };
+
+  const showCategories = () => {
+    return categories.map((c, i) => {
+      return (
+        <button
+          onDoubleClick={() => deleteConfirm(c.slug)}
+          title="Double click to delete"
+          key={i}
+          className="btn btn-outline-primary mr-1 ml-1 mt-3"
+        >
+          {c.name}
+        </button>
+      );
+    });
+  };
+
+  const deleteConfirm = slug => {
+    let answer = window.confirm(
+      "Are you sure you want to delete this category?"
+    );
+    if (answer) {
+      deleteCategory(slug);
+    }
+  };
+
+  const deleteCategory = slug => {
+    // console.log('delete', slug);
+    removeCategory(slug, token).then(data => {
+      if (data.error) {
+        console.log(data.error);
+        setValues({ ...values, error: true, success: false });
+      } else {
+        setValues({
+          ...values,
+          error: false,
+          success: false,
+          name: "",
+          removed: true,
+          reload: !reload
+        });
+      }
+    });
+  };
+
   const clickSubmit = e => {
     e.preventDefault();
-    // console.log("create category", name);
+    // console.log('create category', name);
     create({ name }, token).then(data => {
       if (data.error) {
-        setValues({ ...values, error: data.error, success: false });
+        setValues({ ...values, error: true, success: false });
       } else {
-        setValues({ ...values, error: false, success: true, name: "" });
+        setValues({
+          ...values,
+          error: false,
+          success: true,
+          name: "",
+          removed: false,
+          reload: !reload
+        });
       }
     });
   };
@@ -38,7 +108,29 @@ const Category = () => {
     });
   };
 
-  const newCategoryForm = () => (
+  const showSuccess = () => {
+    if (success) {
+      return <p className="text-success">Category is created</p>;
+    }
+  };
+
+  const showError = () => {
+    if (error) {
+      return <p className="text-danger">Category already exist</p>;
+    }
+  };
+
+  const showRemoved = () => {
+    if (removed) {
+      return <p className="text-danger">Category is removed</p>;
+    }
+  };
+
+  const mouseMoveHandler = e => {
+    setValues({ ...values, error: false, success: false, removed: "" });
+  };
+
+  const newCategoryFom = () => (
     <form onSubmit={clickSubmit}>
       <div className="form-group">
         <label className="text-muted">Name</label>
@@ -51,14 +143,24 @@ const Category = () => {
         />
       </div>
       <div>
-        <button className="btn btn-primary" type="submit">
+        <button type="submit" className="btn btn-primary">
           Create
         </button>
       </div>
     </form>
   );
 
-  return <Fragment>{newCategoryForm()}</Fragment>;
+  return (
+    <React.Fragment>
+      {showSuccess()}
+      {showError()}
+      {showRemoved()}
+      <div onMouseMove={mouseMoveHandler}>
+        {newCategoryFom()}
+        {showCategories()}
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default Category;
