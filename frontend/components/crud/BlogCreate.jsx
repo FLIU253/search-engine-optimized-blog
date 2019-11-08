b0f6a8b25338e3e0c2467f79c94eb50ab2cfc945
Franklin Liu <franklin.liu@autodata.net>
2019-11-08 11:10:16 -0500

    blog create page setup

---
+++ frontend/components/crud/BlogCreate.jsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import { useState, useEffect } from "react";
+import Router, { withRouter } from "next/router";
+import dynamic from "next/dynamic";
+import { getCookie, isAuth } from "../../actions/auth";
+import { getCategories } from "../../actions/category";
+import { getTags } from "../../actions/tag";
+import { createBlog } from "../../actions/blog";
+const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
+import "../../node_modules/react-quill/dist/quill.snow.css";
+
+const CreateBlog = ({ router }) => {
+  return (
+    <div>
+      <h2>Create blog form</h2>
+      {JSON.stringify(router)}
+    </div>
+  );
+};
+export default withRouter(CreateBlog);
