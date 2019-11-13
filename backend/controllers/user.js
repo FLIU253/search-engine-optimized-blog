a87ce46de482d472df3b5ffbc7426be6a5862586
Franklin Liu <franklin.liu@autodata.net>
2019-11-13 08:41:48 -0500

    updating profile component functionality implemented

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,9 +1,9 @@
 const User = require("../models/user");
 const Blog = require("../models/blog");
-const { errorHandler } = require("../helpers/dbErrorHandler");
 const _ = require("lodash");
 const formidable = require("formidable");
 const fs = require("fs");
+const { errorHandler } = require("../helpers/dbErrorHandler");
 
 exports.read = (req, res) => {
   req.profile.hashed_password = undefined;
@@ -49,6 +49,7 @@ exports.publicProfile = (req, res) => {
 
 exports.update = (req, res) => {
   let form = new formidable.IncomingForm();
+  form.keepExtension = true;
   form.parse(req, (err, fields, files) => {
     if (err) {
       return res.status(400).json({
@@ -66,17 +67,17 @@ exports.update = (req, res) => {
       }
       user.photo.data = fs.readFileSync(files.photo.path);
       user.photo.contentType = files.photo.type;
-
-      user.save((err, result) => {
-        if (err) {
-          return res.status(400).json({
-            error: errorHandler(err)
-          });
-        }
-        user.hashed_password = undefined;
-        res.json(user);
-      });
     }
+
+    user.save((err, result) => {
+      if (err) {
+        return res.status(400).json({
+          error: errorHandler(err)
+        });
+      }
+      user.hashed_password = undefined;
+      res.json(user);
+    });
   });
 };
 
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { requiresSignin, authMiddleware } = require("../controllers/auth");
 const { read, publicProfile, update, photo } = require("../controllers/user");
 
-router.get("/profile", requiresSignin, authMiddleware, read);
+router.get("/user/profile", requiresSignin, authMiddleware, read);
 router.get("/user/:username", publicProfile);
 router.put("/user/update", requiresSignin, authMiddleware, update);
 router.get("/user/photo/:username", photo);
