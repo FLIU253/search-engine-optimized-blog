eec333f64b80104440003b93aea691b4d51479dc
Franklin Liu <franklin.liu@autodata.net>
2019-11-13 08:31:55 -0500

    setting up profile update for regular user

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { requiresSignin, authMiddleware } = require("../controllers/auth");
-const { read, publicProfile } = require("../controllers/user");
+const { read, publicProfile, update, photo } = require("../controllers/user");
 
 router.get("/profile", requiresSignin, authMiddleware, read);
 router.get("/user/:username", publicProfile);
+router.put("/user/update", requiresSignin, authMiddleware, update);
+router.get("/user/photo/:username", photo);
 
 module.exports = router;
